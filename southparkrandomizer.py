cc81327253a31ca6b76846f6d969149a6ff94531
Dektroy <antoine.bardy@gmail.com>
2014-05-03 03:54:55 +0200

    v1.0
    The basic script. I may add/change a few things but this works great !

---
+++ southparkrandomizer.py
@@ -0,0 +1,27 @@
+from random import randint
+import winsound
+import webbrowser
+
+test = ""
+
+#while not test:
+
+saison = randint(1, 17)
+episode = 0
+if saison == 1:
+	episode = randint(1, 13)
+elif saison == 2:
+	episode = randint(1, 18)
+elif saison in [3, 4, 6]:
+	episode = randint(1, 17)
+elif saison == 7:
+	episode = randint(1, 15)
+elif saison == 17:
+	episode = randint(1, 10)
+else:
+	episode = randint(1, 14)
+	#print("Saison : " + str(saison) + ", episode : " + str(episode))
+webbrowser.open('http://south-park-streaming.com/saison-' + str(saison) + "/episode-" + str(episode) + "/*")
+winsound.Beep(saison * 50, 50)
+winsound.Beep(episode * 50, 50)
+	#test = raw_input("")
